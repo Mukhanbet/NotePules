0c0699c92aa2fc213fcf00c6d0c0851514301a4e
Karimova1410 <138432927+Karimova1410@users.noreply.github.com>
2023-12-13 18:01:09 +0600

    added countdown timer for notes

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import json
 from ttkbootstrap import Style
+from datetime import timedelta
 
 # Create the main window
 root = tk.Tk()
 root.title("Notes App")
-root.geometry("500x500")
+root.geometry("600x500")
 style = Style(theme='journal')
-style = ttk.Style()
-
-# Configure the tab font to be bold
 style.configure("TNotebook.Tab", font=("TkDefaultFont", 14, "bold"))
 
-# Create the notebook to hold the notes
 notebook = ttk.Notebook(root, style="TNotebook")
 
-# Load saved notes
+#here we are saving the notes
 notes = {}
 try:
     with open("notes.json", "r") as f:
@@ -24,18 +21,61 @@ try:
 except FileNotFoundError:
     pass
 
-# Create the notebook to hold the notes
+#creating notebook
 notebook = ttk.Notebook(root)
 notebook.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
+timer_frame = ttk.Frame(root)
+timer_frame.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.Y)
 
-# Create a function to add a new note
+
+timer_label = ttk.Label(timer_frame, text="Set Reminder Time (HH:MM:SS):")
+timer_label.pack(pady=5)
+
+
+reminder_entry = ttk.Entry(timer_frame)
+reminder_entry.pack(pady=5)
+
+# timer function
+def start_timer():
+    reminder_time_str = reminder_entry.get()
+    try:
+        reminder_time = timedelta(
+            hours=int(reminder_time_str.split(":")[0]),
+            minutes=int(reminder_time_str.split(":")[1]),
+            seconds=int(reminder_time_str.split(":")[2])
+        )
+    except (ValueError, IndexError):
+        messagebox.showerror("Invalid Input", "Please enter a valid time in HH:MM:SS format.")
+        return
+
+    remaining_time = reminder_time
+
+    def update_timer():
+        nonlocal remaining_time
+        timer_label.config(text=f"Time remaining: {remaining_time}")
+        remaining_time -= timedelta(seconds=1)
+
+        if remaining_time >= timedelta(0):
+            root.after(1000, update_timer)
+        else:
+            timer_label.config(text="Reminder!")
+            # уведомление
+            messagebox.showinfo("Reminder", "Time is up!")
+
+    update_timer()
+
+
+start_timer_button = ttk.Button(timer_frame, text="Start Timer", command=start_timer)
+start_timer_button.pack(pady=5)
+
+# Adding a new note
 def add_note():
-    # Create a new tab for the note
+    # New tab for the note
     note_frame = ttk.Frame(notebook, padding=10)
     notebook.add(note_frame, text="New Note")
 
-    # Create entry widgets for the title and content of the note
+    # Title
     title_label = ttk.Label(note_frame, text="Title:")
     title_label.grid(row=0, column=0, padx=10, pady=10, sticky="W")
 
@@ -48,31 +88,65 @@ def add_note():
     content_entry = tk.Text(note_frame, width=40, height=10)
     content_entry.grid(row=1, column=1, padx=10, pady=10)
 
-    # Create a function to save the note
+    # Reminder
+    reminder_label = ttk.Label(note_frame, text="Reminder Time (HH:MM:SS):")
+    reminder_label.grid(row=2, column=0, padx=10, pady=10, sticky="W")
+
+    reminder_entry_note = ttk.Entry(note_frame)
+    reminder_entry_note.grid(row=2, column=1, padx=10, pady=10)
+
+    # Saving a note and starting a timer
     def save_note():
-        # Get the title and content of the note
+
         title = title_entry.get()
         content = content_entry.get("1.0", tk.END)
+        reminder_time_note_str = reminder_entry_note.get()
 
-        # Add the note to the notes dictionary
-        notes[title] = content.strip()
+        try:
+            reminder_time_note = timedelta(
+                hours=int(reminder_time_note_str.split(":")[0]),
+                minutes=int(reminder_time_note_str.split(":")[1]),
+                seconds=int(reminder_time_note_str.split(":")[2])
+            )
+        except (ValueError, IndexError):
+            messagebox.showerror("Invalid Input", "Please enter a valid time in HH:MM:SS format.")
+            return
 
-        # Save the notes dictionary to the file
+        # Adding the note to the notes dictionary
+        notes[title] = {"content": content.strip(), "reminder_time": reminder_time_note_str}
+
+        # Saving the notes dictionary to the file
         with open("notes.json", "w") as f:
             json.dump(notes, f)
 
-        # Add the note to the notebook
+        # Adding the note to the notebook
         note_content = tk.Text(notebook, width=40, height=10)
         note_content.insert(tk.END, content)
         notebook.forget(notebook.select())
-        notebook.add(note_content, text=title)
+        notebook.add(note_content, text=f"{title} - Reminder Set")
 
-    # Add a save button to the note frame
-    save_button = ttk.Button(note_frame, text="Save",
-                             command=save_note, style="secondary.TButton")
-    save_button.grid(row=2, column=1, padx=10, pady=10)
+        # Set the countdown timer for the note
+        remaining_time_note = reminder_time_note
 
+        def update_timer_note():
+            nonlocal remaining_time_note
+            notebook.tab(notebook.select(), text=f"{title} - Time remaining: {remaining_time_note}")
+            remaining_time_note -= timedelta(seconds=1)
 
+            if remaining_time_note >= timedelta(0):
+                root.after(1000, update_timer_note)
+            else:
+                notebook.tab(notebook.select(), text=f"{title} - Reminder!")
+                # Added messagebox reminder
+                messagebox.showinfo("Reminder", f"Time is up for \"{title}!\"")
+
+        update_timer_note()
+
+    # Save-button
+    save_button = ttk.Button(note_frame, text="Save", command=save_note, style="secondary.TButton")
+    save_button.grid(row=3, column=1, padx=10, pady=10)
+
+#function to load existing notes
 def load_notes():
     try:
         with open("notes.json", "r") as f:
@@ -89,41 +163,38 @@ def load_notes():
         pass
 
 
-# Call the load_notes function when the app starts
+# Calling a funtion
 load_notes()
 
 
-# Create a function to delete a note
+# Function to delete a note
 def delete_note():
-    # Get the current tab index
     current_tab = notebook.index(notebook.select())
 
-    # Get the title of the note to be deleted
-    note_title = notebook.tab(current_tab, "text")
+    # Get the deleted note's title
+    note_title = notebook.tab(current_tab, "text").split(" - ")[0]
 
-    # Show a confirmation dialog
+    # Confirmation
     confirm = messagebox.askyesno("Delete Note",
                                   f"Are you sure you want to delete {note_title}?")
 
     if confirm:
-        # Remove the note from the notebook
+        # Removing the note from the notebook
         notebook.forget(current_tab)
 
-        # Remove the note from the notes dictionary
+        # Removing the note from the notes dictionary
         notes.pop(note_title)
 
-        # Save the notes dictionary to the file
+        # Saving the notes dictionary to the file
         with open("notes.json", "w") as f:
             json.dump(notes, f)
 
 
-# Add buttons to the main window
-new_button = ttk.Button(root, text="New Note",
-                        command=add_note, style="info.TButton")
+# New button
+new_button = ttk.Button(root, text="New Note", command=add_note, style="info.TButton")
 new_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-delete_button = ttk.Button(root, text="Delete",
-                           command=delete_note, style="primary.TButton")
+delete_button = ttk.Button(root, text="Delete", command=delete_note, style="primary.TButton")
 delete_button.pack(side=tk.LEFT, padx=10, pady=10)
 
 root.mainloop()
